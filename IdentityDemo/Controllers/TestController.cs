7a46783bf9384c84e1ca277aac5a2265153a9dcc
milindrajput <milindrajput@WS-NET-016>
2019-03-01 19:27:57 +0530

    all files

---
+++ IdentityDemo/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityDemo.Controllers
+{
+    //[Authorize]
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Identity()
+        {
+            return Content("We are using Identity");
+        }
+
+        [AllowAnonymous]
+        public ActionResult NonIdentiy()
+        {
+            return Content("We are not using Identity");
+        }
+    }
+}
---
+++ IdentityDemo/Models/auth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace IdentityDemo.Models
+{
+    public static class auth
+    {
+       
+
+        public static bool IsSignedInWithFacebook(this IIdentity identity)
+        {
+            // since the Facebook authentication handler add following claim
+            // we could check to see if user has this claim or not
+            var claimIdent = identity as ClaimsIdentity;
+            return claimIdent != null
+                && claimIdent.HasClaim(c => c.Type == "urn:facebook:name");
+        }
+    }
+}
